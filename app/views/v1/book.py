1777c0718c4cfd4b73d5d6659f45ee9ae0fbb576
Todd Tan <todd.tan@capacity.com>
2023-11-08 17:06:50 -0600

    add endpoint to get one book by uuid

--- app/views/v1/book.py
+++ app/views/v1/book.py
@@ -7,7 +7,14 @@ from app.schemas.book import BookSchema
 
 bp = Blueprint('book', __name__, url_prefix='/v1/books')
 
-@bp.route('')
+
+@bp.route('', methods=['GET'])
 def get_all(page: int = 1, per_page: int = 10):
     books = db.paginate(db.select(Book), page=page, per_page=per_page)
     return BookSchema().dump(books, many=True)
+
+
+@bp.route('/<string:uuid>', methods=['GET'])
+def get_one(uuid: str):
+    book = Book.query.filter(Book.uuid == uuid).one_or_404()
+    return BookSchema().dump(book)
